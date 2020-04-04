6dc50188835f32596b02c3686c5ba0c40e68d2dd
Rendy Febry <rendyfebry@gmail.com>
2020-04-04 18:38:04 +0700

    feat(Init): Create init function

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,9 @@
 
 const yargs = require("yargs");
 const chalk = require("chalk");
+const fs = require("fs");
+
+const DATA_PATH = "./data.json";
 
 const options = yargs.usage("Usage: -c command").option("c", {
   alias: "command",
@@ -12,14 +15,36 @@ const options = yargs.usage("Usage: -c command").option("c", {
 
 // Display Usage
 function usage() {
-  console.log("\nUsage:\ntodos [list|add|delete|help] [optional]\n");
+  console.log("\nUsage:\ntodos [init|list|add|delete|help] [optional]\n");
+  console.log("todos -c init \t\t\t To initialize storage");
   console.log("todos -c list \t\t\t To list items");
   console.log('todos -c add "Text here" \t To add new item');
   console.log("todos -c delete id \t\t To delete item");
   console.log("todos -c help \t\t\t For help\n");
 }
 
+function saveData(data) {
+  // stringify data
+  const dataString = JSON.stringify(data);
+
+  // Write file
+  fs.writeFileSync(DATA_PATH, dataString);
+}
+
+function init() {
+  if (!fs.existsSync(DATA_PATH)) {
+    console.log("Initialising storage.");
+    saveData([]);
+    console.log("Storage created.");
+  } else {
+    console.log("Already initialized");
+  }
+}
+
 switch (options.c) {
+  case "init":
+    init();
+    break;
   case "list":
     console.log("list");
     break;
